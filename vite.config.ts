009c4826b293d692d7182356c9a1c92f7eea0a89
tydonn <ariknurwantoh@gmail.com>
2026-01-17 11:22:20 +0700

    change favicon.png

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ⬅️ penting untuk cPanel (root domain)
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
